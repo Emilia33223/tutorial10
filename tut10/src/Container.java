081083d91df1c55c2fb327ce204ee7b174ad154d
Emilia33223 <s33223@pjwstk.edu.pl>
2025-06-05 18:59:22 +0200

    Project added

---
+++ tut10/src/Container.java
@@ -0,0 +1,18 @@
+public class Container {
+    public int mass;
+    public int height;
+    public int tareWeight;
+    public int weight;
+    public int depth;
+    public int maxPayload;
+
+    public Container(int mass, int height, int tareWeight, int weight, int depth, int maxPayload) {
+        this.mass = mass;
+        this.height = height;
+        this.tareWeight = tareWeight;
+        this.weight = weight;
+        this.depth = depth;
+        this.maxPayload = maxPayload;
+
+    }
+}
