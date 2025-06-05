96ece5170cda4ec210d6451713a5b09e19889928
Emilia33223 <s33223@pjwstk.edu.pl>
2025-06-05 19:19:53 +0200

    Project added

---
+++ tut10/src/GasContainer.java
@@ -0,0 +1,6 @@
+public class GasContainer extends Container implements IHazardNotifier {
+
+    public GasContainer(int mass, int height, int tareWeight, int weight, int depth, int maxPayload) {
+        super(mass, height, tareWeight, weight, depth, maxPayload);
+    }
+}
---
+++ tut10/src/LiquidContainer.java
@@ -0,0 +1,6 @@
+public class LiquidContainer extends Container implements IHazardNotifier{
+
+    public LiquidContainer(int mass, int height, int tareWeight, int weight, int depth, int maxPayload) {
+        super(mass, height, tareWeight, weight, depth, maxPayload);
+    }
+}
